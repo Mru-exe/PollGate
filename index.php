997b1efd7406be3e94929382cd58dbc36e5e821f
pp-maki <martin.kindl@processpoint.cz>
2024-10-29 23:43:01 +0100

    Fix index.php routing

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
-// Define a helper function to load views
+//Function will serve view based on the URI path
 function renderView($viewName) {
-    echo($viewName);
-    // $viewFile = __DIR__ . "/public/{$viewName}.php";
-    // if (file_exists($viewFile)) {
-    //     include $viewFile;
-    // } else {
-    //     include __DIR__ . "/public/404.php";
-    // }
+    $viewFile = __DIR__ . "/public/{$viewName}.php";
+    if (file_exists($viewFile)) {
+        include $viewFile;
+    } else {
+        include __DIR__ . "/public/404.php";
+    }
 }
 
 // Parse the requested path
