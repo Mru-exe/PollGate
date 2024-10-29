e8db2ae188a9f1d299535104b755238d6fbe701a
pp-maki <martin.kindl@processpoint.cz>
2024-10-29 23:14:51 +0100

    Fix Routing

--- index.php
+++ index.php
@@ -1,33 +1,17 @@
 <?php
-// index.php
-
 // Define a helper function to load views
 function renderView($viewName) {
-    $viewFile = __DIR__ . "/public/{$viewName}.php";
-    if (file_exists($viewFile)) {
-        include $viewFile;
-    } else {
-        include __DIR__ . "/public/404.php";
-    }
+    echo($viewName);
+    // $viewFile = __DIR__ . "/public/{$viewName}.php";
+    // if (file_exists($viewFile)) {
+    //     include $viewFile;
+    // } else {
+    //     include __DIR__ . "/public/404.php";
+    // }
 }
 
 // Parse the requested path
-$request = $_GET['page'] ?? 'login';
+$request = $_SERVER['REQUEST_URI'];
+$request = str_replace('/~kindlma7/PollGate/', '', $request);
 
 renderView($request);
-
-// // Handle routing based on the request
-// switch ($request) {
-//     case 'home':
-//         renderView('home');
-//         break;
-//     case 'login':
-//         renderView('about');
-//         break;
-//     case 'contact':
-//         renderView('contact');
-//         break;
-//     default:
-//         renderView('404');  // Show 404 page for undefined routes
-//         break;
-// }
