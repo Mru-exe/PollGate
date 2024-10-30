a85395ef16549e8ff01c486010e713bb889747a8
Mru-exe <martin@kindl.cz>
2024-10-30 10:36:18 +0100

    Add _cbcontext.php

--- public/login.php
+++ public/login.php
@@ -1,3 +1,10 @@
+<?php 
+
+include '../src/config/_cbcontext.php';
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- src/config/_dbcontext.php
+++ src/config/_dbcontext.php
@@ -1,1 +1,15 @@
-    
+<?php
+$database = "mysql:host=" . $_ENV['dbserver'] . ";dbname=" . $_ENV['dbname'];
+$username = $_ENV['dbusername'] ;
+$password = $_ENV['dbpassword'];
+
+echo($database . "\n" . $username . "\n" . $password);
+
+// try {
+//     $conn = new PDO($database, $username, $password);
+//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//     echo "Connection successful";
+//   } catch(PDOException $e) {
+//     echo "Connection failed: " . $e->getMessage();
+//   }
+?>
