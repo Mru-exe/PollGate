6bffed24d0462f9ef12c174fc798307848b9797d
Mru-exe <martin@kindl.cz>
2024-10-30 22:13:45 +0000

    fixed insertion method responsibility

--- src/models/User.php
+++ src/models/User.php
@@ -2,7 +2,7 @@
 
 class User {
     // Properties corresponding to table columns
-    private int $id;
+    private ?int $id;
     private string $username;
     private string $passwordSalt;
     private string $passwordHash;
@@ -13,42 +13,34 @@ class User {
     // private bool $deleted;
 
     public function __construct(array $data = []) {
-        $this->id = $data['id'] ?? 0;
+        $this->id = $data['id'] ?? null;
         $this->username = $data['username'] ?? '';
         $this->passwordSalt = $data['passwordSalt'] ?? '';
         $this->passwordHash = $data['passwordHash'] ?? '';
         $this->roleId = $data['roleId'] ?? null;
         $this->avatarPath = $data['avatarPath'] ?? null;
-        $this->created = new DateTime($data['created'] ?? 'now');
-        $this->modified = isset($data['modified']) ? new DateTime($data['modified']) : null;
-        $this->deleted = $data['deleted'] ?? false;
+        // $this->created = new DateTime($data['created'] ?? 'now');
+        // $this->modified = isset($data['modified']) ? new DateTime($data['modified']) : null;
+        // $this->deleted = $data['deleted'] ?? false;
     }
 
     public function insert(PDO $pdo): int {
+        $sql = "INSERT INTO Users (Username, PasswordSalt, PasswordHash, RoleId, AvatarPath) 
+                VALUES (:username, :passwordSalt, :passwordHash, :roleId, :avatarPath)";
 
-        try {
-            $sql = "INSERT INTO Users (Username, PasswordSalt, PasswordHash, RoleId, AvatarPath) 
-                    VALUES (:username, :passwordSalt, :passwordHash, :roleId, :avatarPath)";
+        $stmt = $pdo->prepare($sql);
+    
+        // Bind values to statement parameters
+        $stmt->bindValue(':username', $this->username, PDO::PARAM_STR);
+        $stmt->bindValue(':passwordSalt', $this->passwordSalt, PDO::PARAM_STR);
+        $stmt->bindValue(':passwordHash', $this->passwordHash, PDO::PARAM_STR);
+        $stmt->bindValue(':roleId', $this->roleId, PDO::PARAM_INT);
+        $stmt->bindValue(':avatarPath', $this->avatarPath, PDO::PARAM_STR);
 
-            $stmt = $pdo->prepare($sql);
-        
-            // Bind values to statement parameters
-            $stmt->bindValue(':username', $this->username, PDO::PARAM_STR);
-            $stmt->bindValue(':passwordSalt', $this->passwordSalt, PDO::PARAM_STR);
-            $stmt->bindValue(':passwordHash', $this->passwordHash, PDO::PARAM_STR);
-            $stmt->bindValue(':roleId', $this->roleId, PDO::PARAM_INT);
-            $stmt->bindValue(':avatarPath', $this->avatarPath, PDO::PARAM_STR);
-
-            // Execute the statement and check for success
-            if ($stmt->execute()) {
-                // Retrieve and return the last inserted ID as an integer
-                return (int)$pdo->lastInsertId();
-            }
-        } catch (\Throwable $th) {
-            if($th->getCode() == 23000){
-                echo 'User already exists';
-            }
-            return 0;
+        // Execute the statement and check for success
+        if($stmt->execute()){
+            // Retrieve and return the last inserted ID as an integer
+            return (int)$pdo->lastInsertId();
         }
         // Return 0 if insertion failed
         return 0;
