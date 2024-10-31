c4f22065d01f9611009750837ba3f872d490d0ce
Mru-exe <martin@kindl.cz>
2024-10-31 17:22:32 +0000

    Added Poll.php

---
+++ src/models/Poll.php
@@ -0,0 +1,26 @@
+<?php
+
+class Poll {
+    private int $id;
+    private string $name;
+    private ?string $description;
+    private string $question;
+    private string $pollTypeCode;
+    private DateTime $created;
+    private int $createdBy;
+    private ?DateTime $modified;
+    private ?int $modifiedBy;
+
+    public function __construct(array $data) {
+        $this->id = $data['id'];
+        $this->name = $data['name'];
+        $this->description = $data['description'] ?? '';
+        $this->question = $data['question'];
+        $this->pollTypeCode = $data['pollTypeCode'];
+        $this->created = $data['created'];
+        $this->createdBy = $data['createdBy'];
+        $this->modified = $data['modified'] ?? null;
+        $this->modifiedBy = $data['modifiedBy'] ?? null;
+    }
+
+}
