f043431307a4d5e88148efe73c93341a67996615
Mru-exe <martin@kindl.cz>
2024-11-20 10:36:38 +0000

    404.php styling structure

--- public/404.php
+++ public/404.php
@@ -4,13 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Not Found</title>
+    <link rel="stylesheet" href="./public/assets/css/main.css">
+    <link rel="stylesheet" href="./public/assets/css/layout.css">
 </head>
 <body>
-    <h1>
-        404
-    </h1>
-    <h2>
-        Page not found... 
-    </h2>
+    <main id="_404">
+        <h1>
+            404
+        </h1>
+        <h2>
+            Page not found... 
+        </h2>
+    </main>
 </body>
 </html>
